c06ead5b2e50b17b0409a17cc5b37e38f0137704
tokihiro.takahashi <magicpot73@gmail.com>
2018-08-27 17:57:18 +0900

    [ADD] first commit S132-Device

---
+++ pca10040/s132/ses/Output/ble_app_template_pca10040_s132 Debug/Obj/ses_nRF_Startup.d
@@ -0,0 +1,2 @@
+Output/ble_app_template_pca10040_s132\ Debug/Obj/ses_nRF_Startup.o: \
+ /Volumes/storage-mac-ssd2/workspace/nRF5_SDK_15.0.0_a53641a/modules/nrfx/mdk/ses_nRF_Startup.s
---
+++ pca10040/s132/ses/Output/ble_app_template_pca10040_s132 Release/Obj/sensorsim.d
@@ -0,0 +1,5 @@
+Output/ble_app_template_pca10040_s132\ Release/Obj/sensorsim.o: \
+ /Volumes/storage-mac-ssd2/workspace/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.c \
+ /Volumes/storage-mac-ssd2/workspace/nRF5_SDK_15.0.0_a53641a/components/libraries/sensorsim/sensorsim.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/include/stdbool.h
